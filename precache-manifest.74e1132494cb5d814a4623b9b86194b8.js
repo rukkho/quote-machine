7a52dc206f018ca0d59838c527f615d260090e14
Solomon <solojohn8@yahoo.com>
2020-10-15 04:46:17 -1100

    Updates

---
+++ precache-manifest.74e1132494cb5d814a4623b9b86194b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d4b404cb714856de0418b3115b7bdc",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "fa1c7f1f32ec2400b3e0",
+    "url": "/quote-machine/static/css/main.5447b478.chunk.css"
+  },
+  {
+    "revision": "d2f82a250ca858e8ca53",
+    "url": "/quote-machine/static/js/2.0f252d38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.0f252d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1c7f1f32ec2400b3e0",
+    "url": "/quote-machine/static/js/main.0456cb2a.chunk.js"
+  },
+  {
+    "revision": "6c1232aa5663ceebc4f7",
+    "url": "/quote-machine/static/js/runtime-main.f2084995.js"
+  }
+]);
